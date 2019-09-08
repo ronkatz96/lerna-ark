039a328ad7ff57bfed09cc8ca460b96e481d258b
yarinvak <yarinvak@gmail.com>
2019-09-08 15:04:16 +0300

    git

--- packages/polaris-middlewares/index.ts
+++ packages/polaris-middlewares/index.ts
@@ -1,24 +1,2 @@
-import {IMiddleware} from "graphql-middleware";
-
-const dataVersionHeaderName: string = "data-version";
-
-const dataVersionMiddleware: IMiddleware = async (resolve: any, root: any, args: any, context: any, info: any) => {
-    const result = await resolve(root, args, context, info);
-    if (!root) { // assert that its a root resolver
-        if (result instanceof Array) {
-            return result.filter(entity => entity.dataVersion ? entity.dataVersion > context.dataVersion : entity);
-        } else {
-            return result;
-        }
-    } else {
-        return result;
-    }
-};
-
-const initContextForDataVersion = async ({req}) => {
-    return {
-        dataVersion: req.headers[dataVersionHeaderName]
-    };
-};
-
+import {dataVersionMiddleware, initContextForDataVersion} from "./data-version/data-version-middleware";
 export {dataVersionMiddleware, initContextForDataVersion};
