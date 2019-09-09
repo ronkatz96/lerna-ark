6080868f2caa6fdd2673e7c10ccb773cf2b10304
yarinvak <yarinvak@gmail.com>
2019-09-09 14:56:37 +0300

    add soft delete tests

---
+++ packages/polaris-middlewares/soft-delete/soft-delete-middleware.test.ts
@@ -0,0 +1,102 @@
+import {expect} from "chai";
+import {softDeletedMiddleware} from "./soft-delete-middleware";
+
+describe('soft delete middleware tests', () => {
+    describe('an array instance', () => {
+        describe('a non root resolver', () => {
+            it('return only non-deleted entities', async () => {
+                const objects = [{title: 'moshe', deleted: false},
+                    {title: 'dani', deleted: true}];
+                const resolve = async (root, args, context, info) => {
+                    return objects;
+                };
+
+                const result = await softDeletedMiddleware(resolve,
+                    {name: 'bla'}, {}, context, {});
+                expect(result).to.deep.equal([{title: 'moshe', deleted: false}]);
+            });
+        });
+        describe('a root resolver', () => {
+            it('return only non-deleted entities', async () => {
+                const objects = [{title: 'moshe', deleted: false},
+                    {title: 'dani', deleted: true}];
+                const resolve = async (root, args, context, info) => {
+                    return objects;
+                };
+
+                const result = await softDeletedMiddleware(resolve,
+                    undefined, {}, context, {});
+                expect(result).to.deep.equal([{title: 'moshe', deleted: false}]);
+            });
+        });
+    });
+
+    describe('a single entity instance', () => {
+        describe('a non root resolver', () => {
+            it('return null if entity is deleted', async () => {
+                const objects = {title: 'moshe', deleted: true};
+                const resolve = async (root, args, context, info) => {
+                    return objects;
+                };
+
+                const result = await softDeletedMiddleware(resolve,
+                    {name: 'bla'}, {}, context, {});
+                expect(result).to.be.null;
+            });
+
+            it('return entity if its not deleted', async () => {
+                const objects = {title: 'moshe', deleted: false};
+                const resolve = async (root, args, context, info) => {
+                    return objects;
+                };
+
+                const result = await softDeletedMiddleware(resolve,
+                    {name: 'bla'}, {}, context, {});
+                expect(result).to.deep.equal({title: 'moshe', deleted: false});
+            });
+            it('return entity if it had no deleted property', async () => {
+                const objects = {title: 'moshe'};
+                const resolve = async (root, args, context, info) => {
+                    return objects;
+                };
+
+                const result = await softDeletedMiddleware(resolve,
+                    {name: 'bla'}, {}, context, {});
+                expect(result).to.deep.equal({title: 'moshe'});
+            });
+        });
+        describe('a root resolver', () => {
+            it('return null if entity is deleted', async () => {
+                const objects = {title: 'moshe', deleted: true};
+                const resolve = async (root, args, context, info) => {
+                    return objects;
+                };
+
+                const result = await softDeletedMiddleware(resolve,
+                    undefined, {}, context, {});
+                expect(result).to.be.null;
+            });
+
+            it('return entity if its not deleted', async () => {
+                const objects = {title: 'moshe', deleted: false};
+                const resolve = async (root, args, context, info) => {
+                    return objects;
+                };
+
+                const result = await softDeletedMiddleware(resolve,
+                    undefined, {}, context, {});
+                expect(result).to.deep.equal({title: 'moshe', deleted: false});
+            });
+            it('return entity if it had no deleted property', async () => {
+                const objects = {title: 'moshe'};
+                const resolve = async (root, args, context, info) => {
+                    return objects;
+                };
+
+                const result = await softDeletedMiddleware(resolve,
+                    undefined, {}, context, {});
+                expect(result).to.deep.equal({title: 'moshe'});
+            });
+        });
+    });
+});
--- packages/polaris-middlewares/soft-delete/soft-delete-middleware.ts
+++ packages/polaris-middlewares/soft-delete/soft-delete-middleware.ts
@@ -1,10 +1,11 @@
-import {IMiddleware} from 'graphql-middleware';
-
-const softDeletedMiddleware: IMiddleware = async (resolve: any, root: any, args: any, context: any, info: any) => {
+const softDeletedMiddleware = async (resolve: any, root: any, args: any, context: any, info: any) => {
     const result = await resolve(root, args, context, info);
     if (result instanceof Array) {
         return result.filter(entity => !entity.deleted);
     } else {
+        if (result.deleted) {
+            return null;
+        }
         return result;
     }
 };
