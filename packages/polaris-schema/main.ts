a30951a836ff449d7b75d0be1c33fc4b077186d9
osher-sade <osher.sade@gmail.com>
2019-10-23 11:05:40 +0300

    added travis support

--- packages/polaris-schema/main.ts
+++
@@ -1,5 +0,0 @@
-export {RepositoryEntity} from './src/common/repository-entity';
-export {repositoryEntityTypeDefs} from './src/common/repository-entity-type-defs';
-export {scalarsResolvers} from './src/scalars/scalars-resolvers';
-export {scalarsTypeDefs} from './src/scalars/scalars-type-defs';
-export {makeExecutablePolarisSchema} from './src/utils/executable-schema-creator';
---
+++ packages/polaris-schema/src/main.ts
@@ -0,0 +1,5 @@
+export {RepositoryEntity} from './common/repository-entity';
+export {repositoryEntityTypeDefs} from './common/repository-entity-type-defs';
+export {scalarsResolvers} from './scalars/scalars-resolvers';
+export {scalarsTypeDefs} from './scalars/scalars-type-defs';
+export {makeExecutablePolarisSchema} from './utils/executable-schema-creator';
