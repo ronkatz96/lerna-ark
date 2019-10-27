c50687c96367e29aafdb7844416e1c3a65b3ee2d
yarinvak <yarinvak@gmail.com>
2019-10-27 12:09:24 +0200

    export connection

--- packages/polaris-typeorm/index.ts
+++ packages/polaris-typeorm/index.ts
@@ -1,4 +1,4 @@
 export {createPolarisConnection} from './src/connections/create-connection'
 export {CommonModel} from './src/models/common-model';
 export {DataVersion} from './src/models/data-version'
-export {ConnectionOptions, Column, Entity} from "typeorm"
+export {ConnectionOptions, Column, Entity, Connection} from "typeorm"
