6559b65be7af58eaee75f7a3ca79ef0f61bb93ef
itaykl <43351063+itaykl@users.noreply.github.com>
2019-12-16 11:32:14 +0200

    Merge pull request #22 from Enigmatis/fix-types-in-typeorm
    fix: fixed subscriber scan

--- packages/polaris-typeorm/src/connections/create-connection.ts
+++ packages/polaris-typeorm/src/connections/create-connection.ts
@@ -16,7 +16,11 @@ export async function createPolarisConnection(
         ? Object.assign(options.extra, configObj)
         : Object.assign(options, { extra: configObj });
     Object.assign(options, {
-        subscribers: ['src/subscribers/*.ts', 'src/subscribers/*.js', options.subscribers],
+        subscribers: [
+            path.resolve(__dirname, '../') + '/subscribers/*.ts',
+            path.resolve(__dirname, '../') + '/subscribers/*.js',
+            options.subscribers,
+        ],
     });
     const connection = await createConnection(options);
     Object.defineProperty(connection, 'manager', { value: new PolarisEntityManager(connection) });
