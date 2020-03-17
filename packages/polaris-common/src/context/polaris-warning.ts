ff80dbd0e2f4f224957cdddfa931d9a1438ad55b
itaykl <itaykl123@gmail.com>
2020-03-17 10:30:07 +0200

    refactor: codeReview changes.

--- packages/polaris-common/src/context/polaris-warning.ts
+++ packages/polaris-common/src/context/polaris-warning.ts
@@ -1,6 +1,3 @@
 import { ApolloError } from 'apollo-server-errors';
 
-export interface PolarisWarning {
-    message: string | ApolloError;
-    path?: string[];
-}
+export type PolarisWarning = ApolloError | string;
