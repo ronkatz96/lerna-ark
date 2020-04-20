72cb616ed84a8dd3b42e24dda56d1ddba6159f33
chenshoo <chenshooker@gmail.com>
2020-04-20 11:47:01 +0300

    Merge pull request #48 from Enigmatis/export-polaris-connection-manager
    fix: export polarisconnectionmanager

--- packages/polaris-typeorm/src/index.ts
+++ packages/polaris-typeorm/src/index.ts
@@ -1,5 +1,8 @@
 export { createPolarisConnection } from './typeorm-bypasses/create-polaris-connection';
-export { getPolarisConnectionManager } from './typeorm-bypasses/polaris-connection-manager';
+export {
+    getPolarisConnectionManager,
+    PolarisConnectionManager,
+} from './typeorm-bypasses/polaris-connection-manager';
 export { PolarisConnection } from './typeorm-bypasses/polaris-connection';
 export { PolarisRepository } from './typeorm-bypasses/polaris-repository';
 export { CommonModel } from './models/common-model';
