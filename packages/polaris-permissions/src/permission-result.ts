75877ccc641107d01af8e6b3ca415ead0587cdda
yairk1 <furmanmail@gmail.com>
2020-06-09 16:22:43 +0300

    preparing for pull request

--- packages/polaris-permissions/src/permission-result.ts
+++
@@ -1,6 +0,0 @@
-export default interface PermissionResult {
-    isPermitted: boolean;
-    responseHeaders?: { [name: string]: string | string[]; }
-    digitalFilters?: { [entity: string]: { [action: string]: any } }
-    portalData?: { [data: string]: any }
-}
